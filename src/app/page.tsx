9c2f486996c17b0c512cb536f716a9921652a774
krhyginne <155836558+krhyginne@users.noreply.github.com>
2025-05-04 00:06:28 -0700

    before creating NavBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
-}
+//homepage
+import Link from 'next/link'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="p-4">
+    <h1>Welcome to my Homepage</h1>
+    <Link href="/about"> About </Link>
+    </div>
+  );  
+};
+
+export default Home;
