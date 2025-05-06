1cc34af42c9cb6ecf7e919bf4a5682c8f6170e01
krhyginne <155836558+krhyginne@users.noreply.github.com>
2025-05-06 12:05:18 -0700

    3.P Making your route with Next.js submission

---
+++ src/app/page.js
@@ -0,0 +1,17 @@
+//homepage
+import Link from 'next/link'
+import React from 'react'
+import Image from "next/image";
+import HeroSection from '@/components/hero';
+import Navbar from '@/components/navbar';
+
+export default function Home() {
+  return (
+    <main className='flex min-h-screen flex-col bg-[#121212]'>
+      <Navbar/>
+      <div className="container mx-auto px-12 py-4">
+        <HeroSection/>
+      </div>
+    </main>
+  );
+}
